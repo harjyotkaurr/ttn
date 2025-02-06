5d4cb64d10cda460a90d13054af678e6c5d7f5d4
harjyotkaurr <harjyotk99@gmail.com>
2025-02-06 17:37:32 +0530

    updated css

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ function toggleMenu() {
     const nav = document.querySelector('nav');
     nav.classList.toggle('active');
 }
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    document.getElementById('confirmationModal').style.display = 'block';
+    document.body.classList.add('modal-open');
+});
+
+function closeModal() {
+    document.getElementById('confirmationModal').style.display = 'none';
+    document.body.classList.remove('modal-open');
+}
+
+function proceedForm() {
+    closeModal();
+    document.getElementById('contactForm').submit();
+}
